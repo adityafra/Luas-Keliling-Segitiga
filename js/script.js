f181276f2c084fd338e0951af2a70da946878c22
unknown <adityaafria606@gmail.com>
2023-10-14 16:14:37 +0700

    second commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function hasilluassegitiga(){
+    let alas = parseFloat(document.getElementById("alas-s").value);
+    let tinggi = parseFloat(document.getElementById("tinggi-s").value);
+    let luas = 0.5 * alas * tinggi;
+    document.getElementById("luas-s").value = luas;
+}
+function hasilkelilingsegitiga(){
+    let sisiA = parseFloat(document.getElementById("sisi-a").value);
+    let sisiB = parseFloat(document.getElementById("sisi-b").value);
+    let sisiC = parseFloat(document.getElementById("sisi-c").value);
+    let keliling = sisiA + sisiB + sisiC;
+    document.getElementById("keliling-s").value = keliling;
+}
+
+function resetsegitiga(){
+    document.getElementById("alas-s").value = "";
+    document.getElementById("tinggi-s").value = "";
+    document.getElementById("luas-s").value = "";
+    document.getElementById("sisi-a").value = "";
+    document.getElementById("sisi-b").value = "";
+    document.getElementById("sisi-c").value = "";
+    document.getElementById("keliling-s").value = "";
+}
